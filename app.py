f25ea1290d099ca459854b201515c8535bb38ab8
SABITABRATA BHATTACHARYA <120928064+Sabitabrata2014@users.noreply.github.com>
2023-04-04 11:22:37 +0000

    Added Application forms on each job and its acknowledgements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify,request
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -21,6 +21,12 @@ def show_job(id):
     return "Not Found",404
   else:
     return render_template('jobpage.html',job = job, company_name="Tushtu")
+
+@app.route('/job/<id>/apply', methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  return render_template('application_submitted.html',application=data,job=job)
   
 
 if __name__ == '__main__':
